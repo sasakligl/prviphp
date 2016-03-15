5419fa886d2e4e5bd314fef6b2ebeb08ad5d784c
Saša Kligl <kligl55@gmail.com>
2016-03-15 18:00:53 +0100

    Dodjelili smo formu na dvije stranice.

--- index.php
+++ index.php
@@ -2,40 +2,25 @@
 <html>
 <head>
     <title>Prvi PHP</title>
-    <meta charset="utf-8"
+    <meta charset="utf-8">
 </head>
 
 <body>
 <h1>Prvi PHP</h1>
-<form>
-    Prvi broj: <br>
+
+<form method="post" action="rezultat.php">
+    Prvi broj:<br>
     <input type="text" name="broj1"><br>
-    Drugi broj: <br>
+    Drugi broj:<br>
     <input type="text" name="broj2"><br>
+    Treći broj:<br>
+    <input type="text" name="broj3"><br>
+    <br>
+    <input type="text" name="ime"><br>
     <br>
     <input type="submit" value="Izračunaj">
-
 </form>
 
-<?php
-    echo('Ovo je prije odlomka<br>');
-    echo('<br>');
-    echo('Super, jos cemo dodati<br>');
-
-    echo (date('d.m.Y.'));
-
-
-
-
-
-
-    $rezultat = $_GET['broj1'] + $_GET['broj2'];
-    echo('<br>' . $rezultat);
-?>
-
-<p>
-    Ovo je odlomak
-</p>
 </body>
 </html>
 
